9a0cdd354f41d9ddb0ed5bb1c487777c5bbb3cc7
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-31 11:38:08 +0700

    make crud service functionally

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,5 +1,10 @@
 import { logger } from '../utils/logger'
 import Product from '../models/product.model'
+import ProductType from '../types/product.types'
+
+export const addProductToDB = async (payload: ProductType) => {
+  return await Product.create(payload)
+}
 
 export const getProductFromDB = async () => {
   return await Product.find()
@@ -10,4 +15,23 @@ export const getProductFromDB = async () => {
       logger.info('Cannot get data from DB')
       logger.error(error)
     })
+}
+
+export const getProductById = async (id: string) => {
+  return await Product.findOne({ product_id: id })
+}
+
+export const updateProductById = async (id: string, payload: ProductType) => {
+  return await Product.findOneAndUpdate(
+    {
+      product_id: id
+    },
+    {
+      $set: payload
+    }
+  )
+}
+
+export const deleteProductById = async (id: string) => {
+  return await Product.findOneAndDelete({ product_id: id })
 }
