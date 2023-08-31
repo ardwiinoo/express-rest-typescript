2f7fd576274108b070ff59a35dcb1c58ee5d4318
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-08-31 11:38:54 +0700

    add product_id and updateProductValidation

--- src/validations/product.validation.ts
+++ src/validations/product.validation.ts
@@ -1,15 +1,21 @@
 import Joi from 'joi'
+import ProductType from '../types/product.types'
 
-interface ProductInterface {
-  name: string
-  price: number
-}
-
-export const createProductValidation = (payload: ProductInterface) => {
+export const createProductValidation = (payload: ProductType) => {
   const schema = Joi.object({
+    product_id: Joi.string().required(),
     name: Joi.string().required(),
     price: Joi.number().allow('', null)
   })
 
   return schema.validate(payload) // error atau value
+}
+
+export const updateProductValidation = (payload: ProductType) => {
+  const schema = Joi.object({
+    name: Joi.string().allow('', null),
+    price: Joi.string().allow('', null)
+  })
+
+  return schema.validate(payload)
 }
