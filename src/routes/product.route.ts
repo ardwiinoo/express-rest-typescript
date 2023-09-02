8576489d0baac410fbe52418bd137699000bed4e
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-09-02 19:24:22 +0700

    use auth middleware for some endpoint

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
 import { createProduct, deleteProduct, getProduct, updateProduct } from '../controllers/product.controller'
+import { requireAdmin, requireUser } from '../middleware/auth'
 
 export const ProductRouter: Router = Router()
 
 ProductRouter.get('/', getProduct)
-ProductRouter.get('/:id', getProduct)
-ProductRouter.post('/', createProduct)
-ProductRouter.put('/:id', updateProduct)
-ProductRouter.delete('/:id', deleteProduct)
+ProductRouter.get('/:id', requireUser, getProduct)
+ProductRouter.post('/', requireAdmin, createProduct)
+ProductRouter.put('/:id', requireAdmin, updateProduct)
+ProductRouter.delete('/:id', requireAdmin, deleteProduct)
